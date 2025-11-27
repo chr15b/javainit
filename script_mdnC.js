19aef18185877d1d413663a267b47f3c7d0d8e60
chr15b <40733763+chr15b@users.noreply.github.com>
2025-11-27 20:34:46 +0000

    Changed temperature check to AND &&

--- script_mdnC.js
+++ script_mdnC.js
@@ -19,22 +19,17 @@ select.addEventListener("change", setWeather);
 function setWeather() {
     const choice = select.value;
 
-    if (choice === "sunny") {
-        if (temperature < 86) {
+    if (choice === "sunny" && temperature < 86) {
             para.textContent = `It is ${temperature} degrees outside - nice and sunny. Let's go out to the beach, \
         or the park, and get an ice cream.`;
-        } else if (temperature >= 86) {
-            para.textContent =
-        `It is ${temperature} degrees outside - REALLY HOT! If you want to go \
+    } else if (choice === "sunny" && temperature >= 86) {
+            para.textContent = `It is ${temperature} degrees outside - REALLY HOT! If you want to go \
         outside, make sure to put some sunscreen on.`;
-        }
     } else if (choice === "rainy") {
-        para.textContent =
-        "Rain is falling outside; take a rain coat and an umbrella, and \
+        para.textContent = "Rain is falling outside; take a rain coat and an umbrella, and \
         don't stay out for too long.";
     } else if (choice === "snowing") {
-        para.textContent = 
-        "The snow is coming down - it is freezing! Best to stay in with a \
+        para.textContent = "The snow is coming down - it is freezing! Best to stay in with a \
         cup of hot chocolate, or go build a snowman."
     } else if (choice === "overcast") {
         para.textContent =
