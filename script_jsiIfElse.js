bb06349d004011805c3447a00876c6751fdd8947
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-01 08:12:51 +0000

    First if statement added

---
+++ script_jsiIfElse.js
@@ -0,0 +1,5 @@
+// Code to support work from https://javascript.info/ifelse
+
+let year = prompt('In which year was ECMAScript-2015 specification published?','');
+
+if (year == 2015) alert( 'You are right!');
