f91159e2f8f0ae2e3170938775e6799fb5fe6065
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-04 22:39:50 +0000

    Added debug(function)

--- devtools.js
+++ devtools.js
@@ -2,3 +2,10 @@ console.log('a');
 console.log('b');
 debugger;
 console.log('c');
+
+function sum(a, b) {
+  let result = a + b; // DevTools pauses on this line.
+  return result;
+}
+debug(sum); // Pass the function object, not a string.
+sum();
