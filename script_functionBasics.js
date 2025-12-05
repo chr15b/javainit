c883286ea8bd13044c943f371bd06a8162837a7e
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-05 20:58:35 +0000

    Added prompt to animal

--- script_functionBasics.js
+++ script_functionBasics.js
@@ -2,5 +2,7 @@ function favouriteAnimal(animal) {
     return animal + " is my favourite animal!"
 }
 
-const message = favouriteAnimal('Goat')
+let animal = prompt("What's your favourite animal?","")
+const message = favouriteAnimal(animal)
 console.log(message)
+alert(message);
