6defe1ee0d7a00f17ef6d4e7855e97869188d16b
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-05 21:21:11 +0000

    Added example function with optional parameters

--- script_functionBasics.js
+++ script_functionBasics.js
@@ -1,8 +1,15 @@
-function favouriteAnimal(animal) {
+/* function favouriteAnimal(animal) {
     return animal + " is my favourite animal!"
 }
 
 let animal = prompt("What's your favourite animal?","")
 const message = favouriteAnimal(animal)
 console.log(message)
-alert(message);
+alert(message); */
+
+function hello(name = "Chris") {
+    console.log(`Hello ${name}!`);
+}
+
+hello("boss");
+hello();
