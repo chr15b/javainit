aae55f1891816dcd1f4c974793d89024938efe77
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-08 21:46:44 +0000

    showMessage v2 added to demonstrate local variables

--- script_jsiFunctions.js
+++ script_jsiFunctions.js
@@ -1,6 +1,16 @@
-function showMessage() {
+/*function showMessage() {
     alert( 'Hello everyone!')
 }
 
 showMessage();
+showMessage(); */
+
+function showMessage() {
+    let message = "Hello, I'm JavaScript!";
+
+    alert( message);
+}
+
 showMessage();
+
+alert( message );
