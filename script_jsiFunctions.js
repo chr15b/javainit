08becd6d2502b6da4f25dd542f4ccd30fad5ec1e
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-09 22:45:05 +0000

    Added parameters example 7

--- script_jsiFunctions.js
+++ script_jsiFunctions.js
@@ -40,9 +40,25 @@ showCount(0);
 showCount(null);
 showCount();*/
 
-function sum(a, b) {
+/* function sum(a, b) {
     return a + b;
 }
 
 let result = sum(1, 2);
-alert( result );
+alert( result ); */
+
+function checkAge(age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return confirm("Do you have permission from your parents?");
+    }
+}
+
+let age = prompt('How old are you?, 18');
+
+if ( checkAge(age) ) {
+    alert( 'Access granted');
+} else {
+    alert( 'Access denied');
+}
