20986b9e04f66d0be7eea233d1c7ae0c733900d9
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-12 21:41:16 +0000

    Added callback function

--- script_jsiFunctionExpressions.js
+++ script_jsiFunctionExpressions.js
@@ -1,3 +1,5 @@
+/* Initial function expressions
+
 function sayHi() {
     alert( "Hello");
 }
@@ -6,3 +8,21 @@ let func = sayHi;
 
 func();
 sayHi();
+*/
+
+// Callback function
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+        else no();
+}
+
+function showOk() {
+    alert( "You agreed.");
+}
+
+function showCancel() {
+    alert( "You cancelled the execution.");
+}
+
+ask("Do you agree?", showOk, showCancel);
