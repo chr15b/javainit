e82c240b99b7f58ee38fbd4c24c2092025ba3c5c
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-13 20:46:55 +0000

    Added example 3

--- script_jsiArrowFunctions.js
+++ script_jsiArrowFunctions.js
@@ -6,8 +6,20 @@ alert( sum(1,2));
 
 */
 
-// Example 2
+/* Example 2
 
 let double = n => n * 2;
 
 alert(double(3));
+
+*/
+
+// Example 3
+
+let age = prompt("What is your age?",18);
+
+let welcome = (age < 18) ?
+    () => alert("Hello!") :
+    () => alert("Greetings!");
+
+welcome();
