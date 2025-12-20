61d3ba554edaf648af4c46798ba8abe5f3a64900
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-20 21:41:06 +0000

    Example 2 added

--- script_understandingErrors.js
+++ script_understandingErrors.js
@@ -1,6 +1,21 @@
-//Example 1
+/*Example 1
 
 const a = "Hello";
 const b = "World";
 
 console.log(c);
+*/
+
+//Example 2
+const a = 5;
+const b = 10;
+
+function add() {
+    return c;
+}
+
+function print() {
+    add();
+}
+
+print();
