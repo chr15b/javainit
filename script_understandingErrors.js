0c3ef26b539863aca15ec71b6cc0451580e0da94
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-20 21:45:43 +0000

    Example 4 added

--- script_understandingErrors.js
+++ script_understandingErrors.js
@@ -21,8 +21,17 @@ function print() {
 print();
 */
 
-//Example 3
+/*Example 3
 
 function helloWorld() {
     console.log "Hello World!"
 }
+*/
+
+//Example 4
+
+const str1 = "Hello";
+
+const str2 = "World";
+
+const message = str1.push(str2);
